9c06524d5df306f737b734b414dd6f6dc4288291
ziye0229 <504250110@qq.com>
2019-12-18 19:48:09 +0800

    Update forwarding.cpp

--- Router-Lab/Homework/forwarding/forwarding.cpp
+++ Router-Lab/Homework/forwarding/forwarding.cpp
@@ -10,6 +10,9 @@
  * @param len 即 packet 的长度，单位为字节
  * @return 校验和无误则返回 true ，有误则返回 false
  */
+
+bool validateIPChecksum(uint8_t *packet, size_t len);
+
 bool forward(uint8_t *packet, size_t len) {
   // TODO:
   uint8_t *p;
@@ -36,6 +39,40 @@ bool forward(uint8_t *packet, size_t len) {
       cksum = (uint16_t)(~cksum);
       packet[10] = uint8_t(cksum>>8);
       packet[11] = uint8_t(cksum&0x00ff);
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+  else
+  {
+    return false;
+  }
+}
+
+bool validateIPChecksum(uint8_t *packet, size_t len) {
+  // TODO:
+  uint8_t *p;
+  int headLen;
+  unsigned long cksum = 0;
+  p = packet + 2;
+  if(int((p[0]&0x0f)<<2) <= len)  //IP总长度
+  {
+    p = packet;
+    headLen = int((p[0]&0x0f)<<2);
+    while (headLen > 0)
+    {
+      cksum += (uint16_t(p[0]<<8) + uint16_t(p[1]));
+      p += 2;
+      headLen -= 2;
+    }
+    cksum = (cksum>>16) + (cksum&0xffff);
+    cksum += (cksum>>16);
+    cksum = uint16_t(cksum&0xffff);
+    if (uint16_t(cksum) == 0xffff)
+    {
       return true;
     }
     else
